e512ee82e70ba19b2aa42389d767c7dac4fd722f
Daniel Duggin <46377670+danielduggin2@users.noreply.github.com>
2024-01-24 23:33:20 -0500

    File Revamp

---
+++ website/src/App.js
@@ -0,0 +1,11 @@
+import NavBar from "./Homepage/homepage";
+
+function App(){
+    return(
+        <div>
+            <NavBar/>
+        </div>
+    )
+}
+
+export default App;
---
+++ website/src/Homepage/homepage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './homepage.css'
+const NavBar = () => (
+    <div id="nav-bar">
+    <input id="nav-toggle" type="checkbox"/>
+    <div id="nav-header"><a id="nav-title" href="https://codepen.io" target="_blank">C<i class="fab fa-codepen"></i>DEPEN</a>
+        <label for="nav-toggle"><span id="nav-toggle-burger"></span></label>
+        <hr/>
+    </div>
+    <div id="nav-content">
+        <div class="nav-button"><i class="fas fa-palette"></i><span>Your Work</span></div>
+        <div class="nav-button"><i class="fas fa-images"></i><span>Assets</span></div>
+        <div class="nav-button"><i class="fas fa-thumbtack"></i><span>Pinned Items</span></div>
+        <hr/>
+        <div class="nav-button"><i class="fas fa-heart"></i><span>Following</span></div>
+        <div class="nav-button"><i class="fas fa-chart-line"></i><span>Trending</span></div>
+        <div class="nav-button"><i class="fas fa-fire"></i><span>Challenges</span></div>
+        <div class="nav-button"><i class="fas fa-magic"></i><span>Spark</span></div>
+        <hr/>
+        <div class="nav-button"><i class="fas fa-gem"></i><span>Codepen Pro</span></div>
+        <div id="nav-content-highlight"></div>
+    </div>
+    <input id="nav-footer-toggle" type="checkbox"/>
+    <div id="nav-footer">
+
+    </div>
+    </div>
+);
+
+export default NavBar;
