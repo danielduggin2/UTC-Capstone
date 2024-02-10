ffb8762887b24ffe73266ee761420b9ed6f3b24e
Daniel Duggin <46377670+danielduggin2@users.noreply.github.com>
2024-02-09 20:06:34 -0500

    Simplified dashboard

--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -1,20 +1,20 @@
-import { faker } from '@faker-js/faker';
+// import { faker } from '@faker-js/faker';
 
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
 
-import Iconify from 'src/components/iconify';
+// import Iconify from 'src/components/iconify';
 
 import AppTasks from '../app-tasks';
-import AppNewsUpdate from '../app-news-update';
-import AppOrderTimeline from '../app-order-timeline';
-import AppCurrentVisits from '../app-current-visits';
-import AppWebsiteVisits from '../app-website-visits';
-import AppWidgetSummary from '../app-widget-summary';
-import AppTrafficBySite from '../app-traffic-by-site';
-import AppCurrentSubject from '../app-current-subject';
-import AppConversionRates from '../app-conversion-rates';
+// import AppNewsUpdate from '../app-news-update';
+// import AppOrderTimeline from '../app-order-timeline';
+// import AppCurrentVisits from '../app-current-visits';
+// import AppWebsiteVisits from '../app-website-visits';
+// import AppWidgetSummary from '../app-widget-summary';
+// import AppTrafficBySite from '../app-traffic-by-site';
+// import AppCurrentSubject from '../app-current-subject';
+// import AppConversionRates from '../app-conversion-rates';
 
 // ----------------------------------------------------------------------
 
@@ -27,43 +27,43 @@ export default function AppView() {
 
       <Grid container spacing={3}>
         <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
+          {/* <AppWidgetSummary
             title="Weekly Sales"
             total={714000}
             color="success"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
-          />
+          /> */}
         </Grid>
 
         <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
+          {/* <AppWidgetSummary
             title="New Users"
             total={1352831}
             color="info"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
-          />
+          /> */}
         </Grid>
 
         <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
+          {/* <AppWidgetSummary
             title="Item Orders"
             total={1723315}
             color="warning"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
-          />
+          /> */}
         </Grid>
 
         <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
+          {/* <AppWidgetSummary
             title="Bug Reports"
             total={234}
             color="error"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
-          />
+          /> */}
         </Grid>
 
         <Grid xs={12} md={6} lg={8}>
-          <AppWebsiteVisits
+          {/* <AppWebsiteVisits
             title="Website Visits"
             subheader="(+43%) than last year"
             chart={{
@@ -101,11 +101,11 @@ export default function AppView() {
                 },
               ],
             }}
-          />
+          /> */}
         </Grid>
 
         <Grid xs={12} md={6} lg={4}>
-          <AppCurrentVisits
+          {/* <AppCurrentVisits
             title="Current Visits"
             chart={{
               series: [
@@ -115,11 +115,11 @@ export default function AppView() {
                 { label: 'Africa', value: 4443 },
               ],
             }}
-          />
+          /> */}
         </Grid>
 
         <Grid xs={12} md={6} lg={8}>
-          <AppConversionRates
+          {/* <AppConversionRates
             title="Conversion Rates"
             subheader="(+43%) than last year"
             chart={{
@@ -136,11 +136,11 @@ export default function AppView() {
                 { label: 'United Kingdom', value: 1380 },
               ],
             }}
-          />
+          /> */}
         </Grid>
 
         <Grid xs={12} md={6} lg={4}>
-          <AppCurrentSubject
+          {/* <AppCurrentSubject
             title="Current Subject"
             chart={{
               categories: ['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math'],
@@ -150,11 +150,11 @@ export default function AppView() {
                 { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
               ],
             }}
-          />
+          /> */}
         </Grid>
 
         <Grid xs={12} md={6} lg={8}>
-          <AppNewsUpdate
+          {/* <AppNewsUpdate
             title="News Update"
             list={[...Array(5)].map((_, index) => ({
               id: faker.string.uuid(),
@@ -163,11 +163,11 @@ export default function AppView() {
               image: `/assets/images/covers/cover_${index + 1}.jpg`,
               postedAt: faker.date.recent(),
             }))}
-          />
+          /> */}
         </Grid>
 
         <Grid xs={12} md={6} lg={4}>
-          <AppOrderTimeline
+          {/* <AppOrderTimeline
             title="Order Timeline"
             list={[...Array(5)].map((_, index) => ({
               id: faker.string.uuid(),
@@ -181,11 +181,11 @@ export default function AppView() {
               type: `order${index + 1}`,
               time: faker.date.past(),
             }))}
-          />
+          /> */}
         </Grid>
 
         <Grid xs={12} md={6} lg={4}>
-          <AppTrafficBySite
+          {/* <AppTrafficBySite
             title="Traffic by Site"
             list={[
               {
@@ -209,10 +209,10 @@ export default function AppView() {
                 icon: <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={32} />,
               },
             ]}
-          />
+          /> */}
         </Grid>
 
-        <Grid xs={12} md={6} lg={8}>
+        <Grid xs={12} md={6} lg={16}>
           <AppTasks
             title="Tasks"
             list={[
