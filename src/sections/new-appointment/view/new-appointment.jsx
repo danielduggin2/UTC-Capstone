4446b1d5d7951b1674c2c6495a12e415db4fc45b
Bri-Black <zkd766@mocs.utc.edu>
2024-02-15 05:46:35 -0500

    error and bug fixes for new appointment and dashboard

--- src/sections/new-appointment/view/new-appointment.jsx
+++ src/sections/new-appointment/view/new-appointment.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { TextField, Button, Typography, Divider, Grid } from '@mui/material';
+
 import { DatePicker } from '@mui/lab';
+import { Grid, Button, Divider, TextField, Typography } from '@mui/material';
 // import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 // import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 // import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
