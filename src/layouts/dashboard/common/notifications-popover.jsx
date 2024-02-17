a4302a0ab4853e9232e403a37af2b52d15c8d1a0
Daniel Duggin <46377670+danielduggin2@users.noreply.github.com>
2024-02-17 17:46:57 -0500

    Changed Notification Icon for messaging

--- src/layouts/dashboard/common/notifications-popover.jsx
+++ src/layouts/dashboard/common/notifications-popover.jsx
@@ -101,7 +101,7 @@ export default function NotificationsPopover() {
     <>
       <IconButton color={open ? 'primary' : 'default'} onClick={handleOpen}>
         <Badge badgeContent={totalUnRead} color="error">
-          <Iconify width={24} icon="solar:bell-bing-bold-duotone" />
+          <Iconify width={24} icon="nimbus:mail" />
         </Badge>
       </IconButton>
 
