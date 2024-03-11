e3f85dd5536675a8d1ec30b53ee38791898d29fc
Daniel Duggin <46377670+danielduggin2@users.noreply.github.com>
2024-03-11 16:32:01 -0400

    Header update

--- src/layouts/dashboard/header.jsx
+++ src/layouts/dashboard/header.jsx
@@ -34,12 +34,12 @@ export default function Header({ onOpenNav }) {
         </IconButton>
       )}
 
-      <Searchbar />
+      {/* <Searchbar /> */}
 
       <Box sx={{ flexGrow: 1 }} />
 
       <Stack direction="row" alignItems="center" spacing={1}>
-        <LanguagePopover />
+        {/* <LanguagePopover /> */}
         <NotificationsPopover />
         <AccountPopover />
       </Stack>
