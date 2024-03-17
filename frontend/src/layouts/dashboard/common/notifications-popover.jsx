a0d2604b6b962bc226d05ad715e817a70693af6f
AndresCavalie <andrescavalie02@gmail.com>
2024-03-17 18:26:00 -0400

    limit popover to example email

--- frontend/src/layouts/dashboard/common/notifications-popover.jsx
+++ frontend/src/layouts/dashboard/common/notifications-popover.jsx
@@ -28,33 +28,6 @@ import Scrollbar from 'src/components/scrollbar';
 const NOTIFICATIONS = [
     {
         id: faker.string.uuid(),
-        title: 'Your order is placed',
-        description: 'waiting for shipping',
-        avatar: null,
-        type: 'order_placed',
-        createdAt: set(new Date(), { hours: 10, minutes: 30 }),
-        isUnRead: true,
-    },
-    {
-        id: faker.string.uuid(),
-        title: faker.person.fullName(),
-        description: 'answered to your comment on the Minimal',
-        avatar: '/assets/images/avatars/avatar_2.jpg',
-        type: 'friend_interactive',
-        createdAt: sub(new Date(), { hours: 3, minutes: 30 }),
-        isUnRead: true,
-    },
-    {
-        id: faker.string.uuid(),
-        title: 'You have new message',
-        description: '5 unread messages',
-        avatar: null,
-        type: 'chat_message',
-        createdAt: sub(new Date(), { days: 1, hours: 3, minutes: 30 }),
-        isUnRead: false,
-    },
-    {
-        id: faker.string.uuid(),
         title: 'You have new mail',
         description: 'sent from Guido Padberg',
         avatar: null,
@@ -62,15 +35,7 @@ const NOTIFICATIONS = [
         createdAt: sub(new Date(), { days: 2, hours: 3, minutes: 30 }),
         isUnRead: false,
     },
-    {
-        id: faker.string.uuid(),
-        title: 'Delivery processing',
-        description: 'Your order is being shipped',
-        avatar: null,
-        type: 'order_shipped',
-        createdAt: sub(new Date(), { days: 3, hours: 3, minutes: 30 }),
-        isUnRead: false,
-    },
+  
 ];
 
 export default function NotificationsPopover() {
@@ -151,22 +116,6 @@ export default function NotificationsPopover() {
                         }
                     >
                         {notifications.slice(0, 2).map((notification) => (
-                            <NotificationItem key={notification.id} notification={notification} />
-                        ))}
-                    </List>
-
-                    <List
-                        disablePadding
-                        subheader={
-                            <ListSubheader
-                                disableSticky
-                                sx={{ py: 1, px: 2.5, typography: 'overline' }}
-                            >
-                                Before that
-                            </ListSubheader>
-                        }
-                    >
-                        {notifications.slice(2, 5).map((notification) => (
                             <NotificationItem key={notification.id} notification={notification} />
                         ))}
                     </List>
